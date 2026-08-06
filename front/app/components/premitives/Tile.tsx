31eb07737974b7305819b29d8dae4ae75e0771f5
iku nojiri <>
2026-08-06 13:32:49 +0900

    feat: Tile.tsx

---
+++ front/app/components/premitives/Tile.tsx
@@ -0,0 +1,41 @@
+import { ElementType, ReactNode, ComponentProps } from "react";
+import { tv } from "tailwind-variants";
+import type { VariantProps } from "tailwind-variants";
+
+const variants = tv({
+  base: "inline-flex justify-center items-center aspect-square rounded-[10px]",
+  variants: {
+    size: {
+      md: "p-2 w-12",
+      lg: "p-4 w-20",
+    },
+    circle: {
+      true: "rounded-full"
+    },
+    appearance: {
+      primary: "bg-tile-primary",
+      secondary: "bg-tile-secondary",
+      destructive: "bg-destructive",
+    }
+  },
+  defaultVariants: {
+    size: "md",
+    circle: false,
+    appearance: "primary"
+  },
+});
+
+type Props<T extends ElementType> = VariantProps<typeof variants> & {
+  as?: T;
+  children: ReactNode;
+} & Omit<ComponentProps<T>, "children">;
+
+export const Tile = <T extends ElementType = "div">({ as, className, children, size, circle, appearance, ...props }: Props<T>) => {
+  const Tag = as || "div";
+
+  return (
+    <Tag {...props} className={variants({ size, circle, appearance, className})}>
+      {children}
+    </Tag>
+  );
+}
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Container } from "./components/premitives/Container";
 import { Inner } from "./components/premitives/Inner";
+import { Tile } from "./components/premitives/Tile";
 
 export default function Home() {
   return (
@@ -8,21 +9,25 @@ export default function Home() {
       <Container as="section" colored={true}>
         <Inner size="lg">
           <p>ほげ</p>
+          <Tile as="div">a</Tile>
         </Inner>
       </Container>
       <Container>
         <Inner size="md">
           <p>ほげ</p>
+          <Tile as="span" appearance={"secondary"}>a</Tile>
         </Inner>
       </Container>
       <Container colored={true}>
         <Inner size="sm">
           <p>ほげ</p>
+          <Tile as="li" appearance={"destructive"} size="lg">a</Tile>
         </Inner>
       </Container>
       <Container colored={true}>
         <Inner size="xs">
           <p>ほげ</p>
+          <Tile as="p" circle={true}>a</Tile>
         </Inner>
       </Container>
     </>
